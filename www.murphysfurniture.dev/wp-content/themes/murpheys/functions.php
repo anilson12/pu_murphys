f518b59bb088c825262709b4e034a230133098c8
anilson12 <nils8104@pacificu.edu>
2017-02-24 13:31:32 -0800

    Header.php and Sass
    I have finished the header.php and enqueued sass.

---
+++ www.murphysfurniture.dev/wp-content/themes/murpheys/functions.php
@@ -0,0 +1,45 @@
+<?php 
+	
+// 	Adding stylesheets
+function add_theme_style() {
+	wp_enqueue_style( 'simple-grid', get_template_directory_uri() . '/css/simple-grid.min.css',false,'1.0','all');
+	
+	wp_enqueue_style( 'styles', get_template_directory_uri() . '/css/styles.min.css',false,'1.0','all');
+	
+	wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/css/slick-theme.css',false,'1.0','all');
+
+	wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/css/slick.css',false,'1.0','all');
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'add_theme_style' );
+
+// Adding scripts
+function add_theme_script() {
+	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery', 'jquery-ui-core' ), 1.1, true);
+	
+	wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/js/slick.min.js', array('jquery'),'1.6', false);
+
+}
+
+add_action( 'wp_enqueue_scripts', 'add_theme_script' );
+
+//add theme Header Support
+$header_logo = array(
+	'default-image'          => '',
+	'width'                  => 150,
+	'height'                 => 150,
+	'flex-height'            => false,
+	'flex-width'             => false,
+	'uploads'                => true,
+	'random-default'         => false,
+	'header-text'            => true,
+	'default-text-color'     => '',
+	'wp-head-callback'       => '',
+	'admin-head-callback'    => '',
+	'admin-preview-callback' => '',
+);
+
+add_theme_support( 'custom-header', $header_logo );
+
+?>
---
+++ www.murphysfurniture.dev/wp-content/themes/murpheys/index.php
@@ -0,0 +1,22 @@
+<?php get_header() ?>
+<?php get_sidebar() ?>
+
+<div class="wrap">
+<!-- This is the home slider (slickjs) -->
+	
+	<div class="hero-slider">
+	  	<div>
+		  
+		</div>
+		<div>
+
+		</div>
+		<div>
+			<img src="<?php echo get_template_directory_uri().'/images/passing_moments.jpg' ?>"/> 
+
+		</div>
+	</div>
+	
+</div>
+
+<?php get_footer() ?>
